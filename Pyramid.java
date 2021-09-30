67fa40879f6bdf03686a270b59f651a753aa5e4a
jaynesh mehta <82892965+jayneshmehta@users.noreply.github.com>
2021-09-30 20:16:20 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,16 @@
+
+public class Pyramid {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n = 10;
+		for(int i=0; i<=n; i++) {
+    	   for(int j=0;j<=n;j++){
+  if(i<n-j) {
+	  System.out.print(" ");	  
+  }else {
+  System.out.print("* ");
+  }
+    }System.out.println("");
+	}
+ }
+}
